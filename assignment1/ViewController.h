eb9d44580edc24bf31aac4dfd66b0b6e36a671eb
hhouston <hhouston@smu.edu>
2014-01-27 17:25:43 -0600

    Initial Commit

---
+++ assignment1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  assignment1
+//
+//  Created by Hunter Houston on 1/27/14.
+//  Copyright (c) 2014 SMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
